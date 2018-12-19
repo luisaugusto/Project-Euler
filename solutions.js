394f60b7b7b649f3256dc069705fb253b89ed1e1
Luis Augusto <luis.augusto@hdmz.com>
2018-12-19 13:28:36 -0800

    Removed space

--- solutions.js
+++ solutions.js
@@ -178,7 +178,6 @@ console.timeEnd('Problem 7');
 /*
 Problem [008]: Largest product in a series
 The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
-
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
  */
 const largestProduct = (num, len) => {
