a34e7372a69e77f4316f539157de10ee5970a84a
Luis Augusto <luis.augusto@hdmz.com>
2018-12-19 13:37:05 -0800

    Added problem 9 prompt

--- solutions.js
+++ solutions.js
@@ -219,3 +219,13 @@ const num =
 
 console.log(largestProduct(num, 13));
 console.timeEnd('Problem 8');
+
+/*
+Problem [009]: Special Pythagorean triplet
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+ */
