11ea337254a587216cfbb5791933bad1513330c0
Luis Augusto <luis.augusto@hdmz.com>
2018-12-20 15:54:14 -0800

    Finished problems 9-11, optimized problem 7

--- solutions.js
+++ solutions.js
@@ -155,10 +155,13 @@ const findPrime = num => {
   };
 
   while (prime.index < num) {
-    prime.number++;
-    let isPrime = true;
+    if (prime.number > 2) prime.number += 2;
+    else prime.number++;
 
-    for (let x = 2; x < prime.number; x++) {
+    let isPrime = true;
+    let maxFactor = Math.floor(Math.sqrt(prime.number));
+
+    for (let x = 3; x <= maxFactor; x++) {
       if (prime.number % x === 0) {
         isPrime = false;
         break;
@@ -180,21 +183,6 @@ Problem [008]: Largest product in a series
 The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
  */
-const largestProduct = (num, len) => {
-  let largestProduct = 0;
-  const numArray = num.split('');
-
-  numArray.forEach((digit, index) => {
-    if (index + len > num.length) return;
-    const product = numArray.slice(index, index + len).reduce((acc, d) => acc * d, 1);
-    largestProduct = Math.max(product, largestProduct);
-  });
-
-  return largestProduct;
-};
-
-console.time('Problem 8');
-
 const num =
   '73167176531330624919225119674426574742355349194934' +
   '96983520312774506326239578318016984801869478851843' +
@@ -217,6 +205,20 @@ const num =
   '05886116467109405077541002256983155200055935729725' +
   '71636269561882670428252483600823257530420752963450';
 
+const largestProduct = (num, len) => {
+  let largestProduct = 0;
+  const numArray = num.split('');
+
+  numArray.forEach((digit, index) => {
+    if (index + len > num.length) return;
+    const product = numArray.slice(index, index + len).reduce((acc, d) => acc * d, 1);
+    largestProduct = Math.max(product, largestProduct);
+  });
+
+  return largestProduct;
+};
+
+console.time('Problem 8');
 console.log(largestProduct(num, 13));
 console.timeEnd('Problem 8');
 
@@ -225,7 +227,117 @@ Problem [009]: Special Pythagorean triplet
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 a^2 + b^2 = c^2
 For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
  */
+const pythagorean = s => {
+  let product = 0;
+
+  for (let a = 1; a < s; a++) {
+    for (let b = a + 1; b < s; b++) {
+      const c = s - b - a;
+      if (a*a + b*b === c*c) {
+        product = a*b*c;
+        break;
+      }
+    }
+
+    if (product) break;
+  }
+
+  return product;
+};
+
+console.time('Problem 9');
+console.log(pythagorean(1000));
+console.timeEnd('Problem 9');
+
+/*
+Problem [010]: Summation of primes
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+ */
+const sumOfPrimes = max => {
+  let sum = 0;
+  let num = 1;
+
+  while (num < max) {
+    if (num > 2) num += 2;
+    else num++;
+
+    let isPrime = true;
+    let maxFactor = Math.floor(Math.sqrt(num));
+
+    for (let x = 3; x <= maxFactor; x++) {
+      if (num % x === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) sum += num;
+  }
+
+  return sum;
+};
+
+console.time('Problem 10');
+console.log(sumOfPrimes(2000000));
+console.timeEnd('Problem 10');
+
+/*
+Problem [011]: Largest product in a grid
+In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+ */
+const grid =
+  '08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08 ' +
+  '49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00 ' +
+  '81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65 ' +
+  '52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91 ' +
+  '22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80 ' +
+  '24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50 ' +
+  '32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70 ' +
+  '67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21 ' +
+  '24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72 ' +
+  '21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95 ' +
+  '78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92 ' +
+  '16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57 ' +
+  '86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58 ' +
+  '19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40 ' +
+  '04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66 ' +
+  '88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69 ' +
+  '04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36 ' +
+  '20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16 ' +
+  '20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54 ' +
+  '01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48';
+
+const multiply = (total, num) => total * num;
+
+const largestGridProduct = grid => {
+  const numbers = grid.split(' ').map(Number).reduce((acc, num, i) => {
+    const arrayIndex = Math.floor(i / 20);
+    acc[arrayIndex] = acc[arrayIndex] ? [...acc[arrayIndex], num] : [num];
+    return acc;
+  }, []);
+
+  let product = 0;
+
+  numbers.forEach((row, y) => {
+    row.forEach((num, x) => {
+      const rowSets = numbers.slice(y, y + 4);
+      const right = row.slice(x, x + 4).reduce(multiply);
+      const down = rowSets.map(r => r[x]).reduce(multiply);
+      const diagRight = rowSets.map((r, i) => x + i >= r.length ? 1 : r[x + i]).reduce(multiply);
+      const diagLeft = rowSets.map((r, i) => x - i < 0 ? 1 : r[x - i]).reduce(multiply);
+      product = Math.max(product, right, down, diagLeft, diagRight);
+    });
+  });
+
+  return product;
+};
+
+console.time('Problem 11');
+console.log(largestGridProduct(grid));
+console.timeEnd('Problem 11');
