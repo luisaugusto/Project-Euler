7003ed4ea5f8dd460ed209744be1e1ed4e69c9bb
Luis Augusto <hello@luis.codes>
2020-03-10 11:39:58 +0800

    project 15

--- 011 - 020/015 - Lattice Paths/solution.js
+++ 011 - 020/015 - Lattice Paths/solution.js
@@ -1,8 +1,5 @@
 const factorial = number => [...Array(number)].reduce((result, _, index) => result * (index + 1), 1);
-
-const numberRoutes = gridSize => {
-    return factorial(gridSize * 2) / Math.pow(factorial(gridSize), 2);
-};
+const numberRoutes = gridSize => factorial(gridSize * 2) / Math.pow(factorial(gridSize), 2);
 
 console.time('Problem 15');
 console.log(numberRoutes(20));
