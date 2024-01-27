6b9a672a079697104c3ef8f054ccccf260792283
Luis Augusto <hello@luis.app>
2024-01-26 16:18:20 -0800

    clean up code

--- 001 - 010/001 - Multiples of 3 and 5/solution.js
+++ 001 - 010/001 - Multiples of 3 and 5/solution.js
@@ -1,8 +1,8 @@
 console.time('Problem 001');
 let count = 0;
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 1000; i++)
     if (i % 3 === 0 || i % 5 === 0) count += i;
-}
+
 console.log(count)
 console.timeEnd('Problem 001');
--- 001 - 010/001 - Multiples of 3 and 5/solution.py
+++ 001 - 010/001 - Multiples of 3 and 5/solution.py
@@ -2,15 +2,9 @@ import time
 
 start = time.time()
 
-
-def divisible(num):
-    return num % 3 == 0 or num % 5 == 0
-
-
 count = 0
 for i in range(1000):
-    if divisible(i):
-        count += i
+    count += i if i % 3 == 0 or i % 5 == 0 else 0
 
 print(count)
 print((time.time() - start) * 1000, "ms")
