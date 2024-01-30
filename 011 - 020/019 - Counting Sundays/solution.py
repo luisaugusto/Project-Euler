f228488c343c2cba65644c09080ab177b0c0f852
Luis Augusto <hello@luis.app>
2024-01-29 19:06:25 -0800

    problems 18 and 19

---
+++ 011 - 020/019 - Counting Sundays/solution.py
@@ -0,0 +1,42 @@
+from timer import Timer
+
+timer = Timer()
+
+
+def get_feb_days(yr):
+    if yr % 100 == 0 and yr % 400 == 0:
+        return 29
+    elif yr % 4 == 0:
+        return 29
+    else:
+        return 28
+
+
+months_to_days = {
+    0: 31,
+    1: 0,
+    2: 31,
+    3: 30,
+    4: 31,
+    5: 30,
+    6: 31,
+    7: 31,
+    8: 30,
+    9: 31,
+    10: 30,
+    11: 31
+}
+day_count = 0
+num_sundays = 0
+
+for year in range(100):
+    current_year = 1901 + year
+    for month in range(12):
+        num_days = get_feb_days(current_year) if month == 1 else months_to_days[month]
+        for day in range(num_days):
+            day_count += 1
+            if day == 0 and (day_count + 1) % 7 == 0:
+                num_sundays += 1
+
+print(num_sundays)
+timer.end()
